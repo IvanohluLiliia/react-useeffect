837f353c0ca12e41fce9de42d0d8a1d244a13e4f
IvanohluLiliia <liliiaivanohlu@gmail.com>
2025-03-17 18:43:06 +0200

    Lab14_Ivanohlu

---
+++ react-useeffect/src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import ColorBlock from './ColorBlock';  
+function App() {
+  return (
+    <div className="App">
+      <h1>Зміна кольору блока</h1>
+      <ColorBlock />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-useeffect/src/ColorBlock.js
@@ -0,0 +1,31 @@
+// src/ColorBlock.js
+import React, { useState, useEffect } from 'react';
+import './ColorBlock.css';
+
+function ColorBlock() {
+  const [color, setColor] = useState('#440b16');
+
+  
+  const toggleColor = () => {
+    setColor((prevColor) => (prevColor === '#440b16' ? '#ffe69a' : '#440b16'));
+  };
+
+  
+  useEffect(() => {
+    console.log(`Поточний колір: ${color}`);
+  }, [color]);
+
+  return (
+    <div className="color-block-container">
+      <div
+        className="color-block"
+        style={{ backgroundColor: color }}
+      ></div>
+      <button className="color-toggle-button" onClick={toggleColor}>
+        Змінити колір
+      </button>
+    </div>
+  );
+}
+
+export default ColorBlock;
